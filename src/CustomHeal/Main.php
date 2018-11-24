9462b3bf6f8a178500771c6e067dbb3a51a17822
xZolpha <xzolpha@gmail.com>
2018-11-24 23:26:33 +1000

    Add files via upload

---
+++ src/CustomHeal/Main.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace CustomHeal;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+
+class Main{
+
+	public function onEnable(): void{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+		switch(strtolower($label)){
+			case "heal":
+				if(!$sender instanceof Player) return false;
+				if($sender->hasPermission("server.heal")){
+					$sender->setHealth(20);
+					$sender->sendMessage("You've been healed");
+				}else{
+					$sender->sendMessage("Sorry, you do not have permission to use this command");
+
+				}
+				break;
+		}
+		return true;
+	}
+}
